5bf3733e09a266322e7c13e209296f419dfee723
Sakshil Verma <sakshilv01@gmail.com>
2021-08-17 23:34:33 +0530

    Project Files added

---
+++ Project Files/where.js
@@ -0,0 +1,22 @@
+myData = [
+[29.0587757,76.085601, 'Haryana, India'],
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[30.0189275,31.499707, 'AUC Avenue، Road، First New Cairo, Cairo Governorate 11835, Egypt'],
+[33.4242399,-111.9280527, 'Tempe, AZ 85281, USA'],
+[39.3301047,-82.1076022, 'Athens, OH 45701, USA'],
+[28.3588163,75.58802039999999, 'Vidya Vihar, Pilani, Rajasthan 333031, India'],
+[6.891986300000001,3.7181286, 'Ilishan-Remo, Nigeria'],
+[25.2677203,82.99125819999999, 'Ajagara, Varanasi, Uttar Pradesh 221005, India'],
+[12.9503878,77.5022224, 'Mysore Rd, Jnana Bharathi, Bengaluru, Karnataka 560056, India'],
+[31.5488923,-97.1130573, '1311 S 5th St, Waco, TX 76706, USA'],
+[39.9619537,116.3662615, '19 Xinjiekou Outer St, Bei Tai Ping Zhuang, Haidian Qu, Beijing Shi, China, 100875'],
+[53.8938988,27.5460609, 'praspiekt Niezaliežnasci 4, Minsk, Belarus'],
+[44.8184518,20.4575913, 'Studentski trg 1, Beograd, Serbia'],
+[42.5030333,-89.0309048, '700 College St, Beloit, WI 53511, USA'],
+[53.8938988,27.5460609, 'prasp. Niezaliežnasci 4, Minsk, Belarus'],
+[40.7519619,-73.9866071, '1001 6th Ave, New York, NY 10018, USA'],
+[10.6779085,78.74454879999999, 'Palkalaiperur, Tiruchirappalli, Tamil Nadu 620024, India'],
+[42.3504997,-71.1053991, 'Boston, MA 02215, USA'],
+[35.3011298,-120.6582825, '1 Grand Ave, San Luis Obispo, CA 93407, USA']
+];
